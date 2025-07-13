4bb1ce223b6750cfeddbcdc1c1017fb30fd19410
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2025-07-13 17:46:31 +0530

    Add files via upload

---
+++ SONALI/plugins/bot/repo.py
@@ -0,0 +1,49 @@
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from SONALI import app
+from config import BOT_USERNAME
+from SONALI.utils.errors import capture_err
+import httpx 
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+start_txt = """
+✰ 𝗪ᴇʟᴄᴏᴍᴇ ᴛᴏ 𝐛𝐚𝐛𝐲  𝗥ᴇᴘᴏs ✰
+ 
+✰ 𝗥ᴇᴘᴏ ᴛᴏ 𝗡ʜɪ 𝗠ɪʟᴇɢᴀ 𝗬ʜᴀ
+ 
+✰ 𝗣ᴀʜʟᴇ 𝗣ᴀᴘᴀ 𝗕ᴏʟ 𝗥ᴇᴘᴏ 𝗢ᴡɴᴇʀ ᴋᴏ 
+
+✰ || @II_gunjan_singh_II ||
+ 
+✰ 𝗥ᴜɴ 24x7 𝗟ᴀɢ 𝗙ʀᴇᴇ 𝗪ɪᴛʜᴏᴜᴛ 𝗦ᴛᴏᴘ
+ 
+"""
+
+
+
+
+@app.on_message(filters.command("repo"))
+async def start(_, msg):
+    buttons = [
+        [ 
+          InlineKeyboardButton("𝗔ᴅᴅ ᴍᴇ 𝗠ᴀʙʏ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+        ],
+        [
+          InlineKeyboardButton("𝗛ᴇʟᴘ", url="https://t.me/PURVI_SUPPORT"),
+          InlineKeyboardButton("🇬𝐮𝐧𝐣𝐚𝐧🐬", url="https://t.me/II_gunjan_singh_II"),
+          ],
+               [
+                InlineKeyboardButton("Tara music bot", url=f"https://t.me/TaraMusicRobot"),
+],
+[
+InlineKeyboardButton("𝗠ᴀɪɴ 𝗕ᴏᴛ", url=f"https://t.me/TaraMusicRobot"),
+
+        ]]
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await msg.reply_photo(
+        photo="https://telegra.ph/file/9601c451ff3bac3c8e3b8.jpg",
+        caption=start_txt,
+        reply_markup=reply_markup
+    )
