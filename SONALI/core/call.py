78a95a4a03a6ace04e655f6eafd27206db7a3abe
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2025-08-17 20:02:50 +0530

    Update call.py

--- SONALI/core/call.py
+++ SONALI/core/call.py
@@ -31,11 +31,11 @@ from SONALI.utils.database import (
     remove_active_video_chat,
     set_loop,
 )
-from SHUKLAMUSIC.utils.exceptions import AssistantErr
-from SHUKLAMUSIC.utils.formatters import check_duration, seconds_to_min, speed_converter
-from SHUKLAMUSIC.utils.inline.play import stream_markup
-from SHUKLAMUSIC.utils.stream.autoclear import auto_clean
-from SHUKLAMUSIC.utils.thumbnails import get_thumb
+from SONALI.utils.exceptions import AssistantErr
+from SONALI.utils.formatters import check_duration, seconds_to_min, speed_converter
+from SONALI.utils.inline.play import stream_markup
+from SONALI.utils.stream.autoclear import auto_clean
+from SONALI.utils.thumbnails import get_thumb
 from strings import get_string
 
 autoend = {}
