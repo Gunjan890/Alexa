554762ed3105d50bbe7c92a78c4783e79ad68a3d
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2025-08-17 19:26:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -60,8 +60,6 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-)  
-
 YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## E.G https://yt.okflix.
 YT_API_KEY = "xbit_ASGQKI9GGW7KVFY5URFT4F"
 COOKIES_URL=getenv("COOKIES_URL" , "https://gist.githubusercontent.com/sparrow9616/f29fc6588086a3c72d92dd9c03773350/raw/4229f3f4aab4a6693fc0794d136d30f54d67ae85/gistfile1.txt")
